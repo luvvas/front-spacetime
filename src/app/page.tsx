7d68364c399b3a319aaa5633d6683bfb2c8c8c24
luvvas <lucasmachado1@ufpr.br>
2023-05-20 07:02:37 -0300

    feat: add right side structure

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,21 @@
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-4xl font-bold">Hello Word</h1>
-      <h1 className="font-alt text-4xl font-bold">Hello Word</h1>
-    </div>
+    <main className="grid min-h-screen grid-cols-2">
+      {/* Left */}
+      <div></div>
+
+      {/* Right */}
+      <div className="p:16 flex flex-col">
+        <div className="flex flex-1 items-center justify-center">
+          <p className="w-[360px] text-center leading-relaxed">
+            Você ainda não registrou nenhuma lembrança, comece a{" "}
+            <a href="" className="underline hover:text-gray-50">
+              criar agora
+            </a>
+            !
+          </p>
+        </div>
+      </div>
+    </main>
   );
 }
