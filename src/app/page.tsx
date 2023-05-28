be18cca01e5b3be0f77ea9bc6657992a3888119a
luvvas <lucasmachado1@ufpr.br>
2023-05-28 16:37:40 -0300

    feat: add dynamic memories

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,14 @@
 import { cookies } from "next/headers";
+import Image from "next/image";
+import Link from "next/link";
+import dayjs from "dayjs";
+import ptBr from "dayjs/locale/pt-br";
 
 import { api } from "./lib/api";
 import { EmptyMemories } from "./components/EmptyMemories";
+import { ArrowRight } from "lucide-react";
+
+dayjs.locale(ptBr);
 
 interface Memory {
   id: string;
@@ -36,8 +43,26 @@ export default async function Home() {
         return (
           <div key={memory.id} className="space-y-4">
             <time className="-ml-8 flex items-center gap-2 text-sm text-gray-100 before:h-px before:w-5 before:bg-gray-50">
-              {memory.createdAt}
+              {dayjs(memory.createdAt).format("D[ de ]MMM[, ]YYYY")}
             </time>
+            <Image
+              src={memory.coverUrl}
+              alt=""
+              width={592}
+              height={280}
+              className="aspect-video w-full rounded-lg object-cover"
+            />
+            <p className="text-lg leading-relaxed text-gray-100">
+              {memory.excerpt}
+            </p>
+
+            <Link
+              href={`/memories/${memory.id}`}
+              className="flex items-center gap-2 text-sm text-gray-200 hover:text-gray-100"
+            >
+              Ler mais
+              <ArrowRight className="h-4 w-4" />
+            </Link>
           </div>
         );
       })}
